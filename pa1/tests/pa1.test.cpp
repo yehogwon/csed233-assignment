364b919319f87cf190e60848708cf031e7c077f0
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-09-20 19:34:18 +0900

    overall ctest env. and backbone for test

--- pa1/tests/pa1.test.cpp
+++ pa1/tests/pa1.test.cpp
@@ -1,7 +1,31 @@
 #define DOING_CTEST
 #include "pa1.cpp"
 
-int main(void) {
-    // TODO: Implement this function
-    return 0;
+#include <iostream>
+
+int main(int argc, char **argv) {
+    if (argc != 2) { // invalid arguments (requires test name)
+        std::cout << "Invalid arguments" << std::endl;
+        std::cout << "Usage: ./pa1.test.out <test_name>" << std::endl;
+        return -1;
+    }
+
+    std::string test_name = argv[1];
+
+    if (test_name == "Task1") {
+        return 1; // to be implemented
+    } else if (test_name == "Task2") {
+        return 1; // to be implemented
+    } else if (test_name == "Task3") {
+        return 1; // to be implemented
+    } else if (test_name == "Task4") {
+        return 1; // to be implemented
+    } else if (test_name == "Task5") {
+        return 1; // to be implemented
+    } else if (test_name == "Task6") {
+        return 1; // to be implemented
+    } else {
+        std::cout << "Invalid test name" << std::endl;
+        return -2; // invalid test name
+    }
 }
