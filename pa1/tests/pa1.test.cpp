7c419df7cc5cc1aacc64fb66b9dac7c477930235
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-09-21 12:34:57 +0900

    general purpose test fn for no arguments

--- pa1/tests/pa1.test.cpp
+++ pa1/tests/pa1.test.cpp
@@ -2,6 +2,54 @@
 #include "pa1.cpp"
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+
+using function_no_args = void (*)(std::ofstream&);
+
+template <typename T>
+using function_1_args = void (*)(std::ofstream&, T);
+
+inline void lstrip(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+
+inline void rstrip(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+inline void strip(std::string &s) {
+    lstrip(s);
+    rstrip(s);
+}
+
+bool test_no_args(function_no_args fn, const std::string &expected_output) {
+    std::string temp_file = "/tmp/pa1.test." + std::to_string(std::rand());
+    std::ofstream temp_out(temp_file);
+    fn(temp_out);
+    temp_out.close();
+
+    std::ifstream temp_in(temp_file);
+    std::string full_content = "", line;
+    while (std::getline(temp_in, line)) full_content += line;
+    strip(full_content);
+    temp_in.close();
+
+    std::remove(temp_file.c_str());
+    return full_content == expected_output;
+}
+
+template <typename T>
+bool test_1_args(const std::pair<T, std::string> &test_case) {
+    return false;
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) { // invalid arguments (requires test name)
@@ -12,29 +60,10 @@ int main(int argc, char **argv) {
 
     std::string test_name = argv[1];
     
-    std::string temp_file = "/tmp/pa1.test." + std::to_string(std::rand());
-    std::ofstream temp_out(temp_file);
-
     if (test_name == "Task1") {
-        task_1(temp_out);
-        temp_out.close();
-
-        std::ifstream temp_in(temp_file);
-        std::string full_content = "", line;
-        while (std::getline(temp_in, line)) full_content += line;
-        std::cout << full_content << std::endl;
-
-        return full_content == "[Task 1]4\n";
+        return !test_no_args(task_1, "[Task 1]4");
     } else if (test_name == "Task2") {
-        task_2(temp_out);
-        temp_out.close();
-
-        std::ifstream temp_in(temp_file);
-        std::string full_content = "", line;
-        while (std::getline(temp_in, line)) full_content += line;
-        std::cout << full_content << std::endl;
-
-        return full_content == "[Task 2]2\n";
+        return !test_no_args(task_2, "[Task 2]2");
     } else if (test_name == "Task3") {
         return 1; // to be implemented
     } else if (test_name == "Task4") {
