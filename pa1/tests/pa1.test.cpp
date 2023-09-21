0a7e9f2096b1d0e4c2e0792bdb8c80c0a160eef7
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-09-21 12:39:21 +0900

    proper file name for temp. output

--- pa1/tests/pa1.test.cpp
+++ pa1/tests/pa1.test.cpp
@@ -6,11 +6,22 @@
 #include <string>
 #include <algorithm>
 #include <cstdio>
+#include <unistd.h>
 
 using function_no_args = void (*)(std::ofstream&);
 
 template <typename T>
 using function_1_args = void (*)(std::ofstream&, T);
+
+const char *SYMBOLS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const int SYMBOLS_LEN = 62;
+
+std::string random_string(const int len) {
+    std::string rstr;
+    rstr.reserve(len);
+    for (int i = 0; i < len; i++) rstr += SYMBOLS[std::rand() % SYMBOLS_LEN];
+    return rstr;
+}
 
 inline void lstrip(std::string &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
@@ -31,7 +42,7 @@ inline void strip(std::string &s) {
 }
 
 bool test_no_args(function_no_args fn, const std::string &expected_output) {
-    std::string temp_file = "/tmp/pa1.test." + std::to_string(std::rand());
+    std::string temp_file = "/tmp/pa1.test." + random_string(10);
     std::ofstream temp_out(temp_file);
     fn(temp_out);
     temp_out.close();
@@ -58,6 +69,7 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    srand(time(NULL) * getpid());
     std::string test_name = argv[1];
     
     if (test_name == "Task1") {
