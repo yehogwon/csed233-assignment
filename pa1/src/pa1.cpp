bc605193adb70c4ee022d462811e5205896ffd65
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-09-23 14:11:32 +0900

    refactor an error on Task5 (var naming) and memory leakage

--- pa1/src/pa1.cpp
+++ pa1/src/pa1.cpp
@@ -209,6 +209,11 @@ void push(char exp, Top* top){
   top->count++;
 }
 
+void delete_top(Top *top) {
+  while (top->count > 0) pop(top);
+  delete top;
+}
+
 bool MatchingParentheses(string ari_exp) { 
   Top *top = new Top;
   top->count = 0;
@@ -217,8 +222,10 @@ bool MatchingParentheses(string ari_exp) {
     if (c == '(' || c == '{' || c == '[') { // open
       push(c, top);
     } else if (c == ')' || c == '}' || c == ']') { // close
-      if (top->count == 0) return false; // got close, but nothing opened
-      else {
+      if (top->count == 0) { // got close, but nothing opened
+        delete_top(top);
+        return false;
+      } else {
         char top_char = pop(top);
         if (c == ')' && top_char != '(' || c == '}' && top_char != '{' || c == ']' && top_char != '[') return false;
         // return false if the close does not match the most recent open
@@ -226,14 +233,9 @@ bool MatchingParentheses(string ari_exp) {
     }
   }
 
-  if (top->count == 0) {
-    delete top;
-    return true;
-  } else {
-    while (top->count > 0) pop(top);
-    delete top;
-    return false;
-  }
+  bool ret = top->count == 0;
+  delete_top(top);
+  return ret;
 }
 ///////////      End of Implementation      /////////////
 /////////////////////////////////////////////////////////
@@ -303,8 +305,8 @@ void task_5(ofstream &fout, InstructionSequence *instr_seq) {
           int start_ = head;
           int count = 0;
           while (true) {
-            answer += queue[head++];
-            if (queue[head] == ' ' && ++count >= size) break;
+            answer += queue[start_++];
+            if (queue[start_] == ' ' && ++count >= size) break;
           }
           answer += " ";
         } else if (command.compare("size") == 0) {
