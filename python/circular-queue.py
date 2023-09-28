7d48bde5c5314b34a91b58631a301680a7869502
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-09-28 14:34:35 +0900

    fix circular queue max size for test case generation and apply it

--- python/circular-queue.py
+++ python/circular-queue.py
@@ -1,8 +1,9 @@
 from typing import TypeVar, Generic, Optional, List
 import argparse
-from random import randint, choices, random
+from random import randint, choices
 
 CASE_SEP = '**** ****'
+QUEUE_SIZE = 5
 
 commands = ['e', 'd', 'show', 'size']
 weights = [0.3, 0.2, 0.25, 0.25]
@@ -41,17 +42,11 @@ class CircularQueue(Generic[T]):
         return temp
 
     def __repr__(self) -> str:
-        if (self.head == -1):
+        if (self.size == 0):
             return 'empty'
         show_ = ''
-        if (self.tail >= self.head):
-            for i in range(self.head, self.tail + 1):
-                show_ += str(self.queue[i]) + ' '
-        else:
-            for i in range(self.head, self.max_size):
-                show_ += str(self.queue[i]) + ' '
-            for i in range(0, self.tail + 1):
-                show_ += str(self.queue[i]) + ' '
+        for i in range(self.size): 
+            show_ += str(self.queue[(self.head + i) % self.max_size]) + ' '
         return show_
     
     def __len__(self) -> int: 
@@ -63,7 +58,7 @@ def main(args: argparse.Namespace) -> None:
         length = randint(args.min_length, args.max_length)
         case = []
         outputs = []
-        queue = CircularQueue[int](randint(args.min_queue_max_length, args.max_queue_max_length))
+        queue = CircularQueue[int](QUEUE_SIZE)
         for _ in range(length):
             command = choices(commands, weights)[0]
             if command == 'e':
@@ -86,8 +81,6 @@ def main(args: argparse.Namespace) -> None:
 if __name__ == '__main__': 
     parser = argparse.ArgumentParser(description="Test Case Gen for PA1 Task 6: Circular Queue")
     parser.add_argument('-N', type=int, required=True, help='The number of samples to generate')
-    parser.add_argument('--min-queue-max-length', '-mM', type=int, required=True, help='The minimum value of the maximum length of the queue')
-    parser.add_argument('--max-queue-max-length', '-MM', type=int, required=True, help='The maximum value of the maximum length of the queue')
     parser.add_argument('--min-length', '-m', type=int, required=True, help='The minimum length of the string')
     parser.add_argument('--max-length', '-M', type=int, required=True, help='The maximum length of the string')
     args = parser.parse_args()
