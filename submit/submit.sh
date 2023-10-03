392527e5d6c2776f5b488da47d2a72a40be3f898
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-10-03 22:17:25 +0900

    submit.sh works in a more general fashion

--- submit/submit.sh
+++ submit/submit.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# NOTE: Adapt to PA2; it has more than one submission files. 
 BASEDIR=$(dirname "$0") # path to the directory of this script
+
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
+declare -a submission_files=(
+    "src/pa1.cpp" # PA1
+    "src/pa2.cpp src/tree.cpp include/tree.h src/heap.cpp include/heap.h" # PA2
+)
 
 # declare a function that prints a message
 usage_exit() {
@@ -28,10 +37,27 @@ fi
 
 for i in 1 2 3 4
 do
-    pa_name=pa$i
-    pa_path="$BASEDIR/../$pa_name/src/$pa_name.cpp"
-    if test -f "$pa_path"; then
-        echo "PA$i Processing..."
-        sed 's,.*//[ ]SUBMIT:.*,,g' $pa_path > "$BASEDIR/$pa_name.cpp"
+    if [ $i -gt ${#submission_files[@]} ] ; then
+        break
     fi
+
+    echo " ${RED}${BOLD}===== PA$i Processing =====${NORMAL}${GREEN} "
+    
+    pa_name=pa$i
+    pa_dir_path="$BASEDIR/../$pa_name"
+    pa_submission_dir_path="$BASEDIR/$pa_name"
+
+    if ! [ -d $pa_submission_dir_path ] ; then
+        echo "Creating a directory for $pa_name at $pa_submission_dir_path"
+        mkdir -p $pa_submission_dir_path
+    fi
+
+    pa_file_relative_paths=(${submission_files[$i-1]})
+    for file_path in "${pa_file_relative_paths[@]}"
+    do
+        file_name=$(basename $file_path)
+        sed 's,.*//[ ]SUBMIT:.*,,g' "$pa_dir_path/$file_path" > "$pa_submission_dir_path/$file_name"
+    done
+
+    echo "Processing done for $pa_name"
 done
