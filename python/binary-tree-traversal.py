58ed3d45d278a820a7abc41a5532ea466e12fbcf
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-10-06 18:45:50 +0900

    tree traversal test case generation implemented; need to implement random binary tree generation

---
+++ python/binary-tree-traversal.py
@@ -0,0 +1,28 @@
+import importlib
+BT = importlib.import_module('binary-tree')
+globals().update({k: getattr(BT, k) for k in BT.__all__}) # from binary-tree import *
+
+import argparse
+from random import randint
+
+CASE_SEP = '**** ****'
+
+def main(args: argparse.Namespace): 
+    cases = []
+    for _ in range(args.N) :
+        height = randint(args.min_height, args.max_height)
+        root: BinaryNode = generate_random_binary_tree(height)
+        str_tree: str = stringify_binary_tree(root)
+        cases += [
+            (f'{str_tree} {mode}', traverse_binary_tree(root, mode)) for mode in ['preorder', 'inorder', 'postorder', 'levelorder']
+        ]
+    print((f'\n{CASE_SEP}\n').join([f'{tup[0]}\n{tup[1]}' for tup in cases]))
+    print(CASE_SEP)
+
+if __name__ == '__main__': 
+    parser = argparse.ArgumentParser(description='Binary Tree Traversal')
+    parser.add_argument('-N', type=int, required=True, help='The number of trees to generate')
+    parser.add_argument('--min-height', '-m', type=int, required=True, help='The minimum height of the tree')
+    parser.add_argument('--max-height', '-M', type=int, required=True, help='The maximum height of the tree')
+    args = parser.parse_args()
+    main(args)
--- python/binary-tree.py
+++ python/binary-tree.py
@@ -1,11 +1,24 @@
-from typing import TypeVar, Generic, Optional, List, Tuple
+from typing import TypeVar, Generic, Optional, List, Tuple, Union
+
+__all__ = [
+    'BinaryNode', 
+    'traverse_binary_tree', 
+    'stringify_binary_tree',
+    'generate_random_binary_tree'
+]
 
 T = TypeVar('T')
 class BinaryNode(Generic[T]): 
-    def __init__(self, value: Optional[T]=None, left: Optional['BinaryNode[T]']=None, right: Optional['BinaryNode[T]']=None) -> None:
-        self.value: Optional[T] = value
-        self.left: Optional[BinaryNode[T]] = left
-        self.right: Optional[BinaryNode[T]] = right
+    def __init__(self, value: Union[Optional[T], str]=None, left: Optional['BinaryNode[T]']=None, right: Optional['BinaryNode[T]']=None) -> None:
+        if isinstance(value, str) and not left and not right: 
+            _root = construct_from_string(value)
+            self.value = _root.value
+            self.left = _root.left
+            self.right = _root.right
+        else: 
+            self.value: Optional[T] = value
+            self.left: Optional[BinaryNode[T]] = left
+            self.right: Optional[BinaryNode[T]] = right
     
     def num_children(self) -> int:
         return int(self.left is not None) + int(self.right is not None)
@@ -50,16 +63,16 @@ def construct_from_string(s: Optional[str]) -> BinaryNode:
     _value, _left_string, _right_string = _split_binary_tree_format(s)
     return BinaryNode(_value, construct_from_string(_left_string), construct_from_string(_right_string))
 
-def _stringify_binary_tree(root: BinaryNode, mode: str) -> str: 
+def _traverse_binary_tree(root: BinaryNode, mode: str) -> str: 
     assert mode in ['preorder', 'inorder', 'postorder', 'levelorder'], f'Invalid mode: {mode}'
     if root is None: 
         return ''
     if mode == 'preorder':
-        return f'{root.value} {_stringify_binary_tree(root.left, mode)} {_stringify_binary_tree(root.right, mode)}'
+        return f'{root.value} {_traverse_binary_tree(root.left, mode)} {_traverse_binary_tree(root.right, mode)}'
     elif mode == 'inorder':
-        return f'{_stringify_binary_tree(root.left, mode)} {root.value} {_stringify_binary_tree(root.right, mode)}'
+        return f'{_traverse_binary_tree(root.left, mode)} {root.value} {_traverse_binary_tree(root.right, mode)}'
     elif mode == 'postorder':
-        return f'{_stringify_binary_tree(root.left, mode)} {_stringify_binary_tree(root.right, mode)} {root.value}'
+        return f'{_traverse_binary_tree(root.left, mode)} {_traverse_binary_tree(root.right, mode)} {root.value}'
     elif mode == 'levelorder':
         queue: List[BinaryNode] = [root]
         result = ''
@@ -71,5 +84,16 @@ def _stringify_binary_tree(root: BinaryNode, mode: str) -> str:
                 queue.append(node.right)
         return result
 
-def stringify_binary_tree(root: BinaryNode, mode: str) -> str: 
-    return ' '.join(_stringify_binary_tree(root, mode).strip().split())
+def traverse_binary_tree(root: BinaryNode, mode: str) -> str: 
+    return ' '.join(_traverse_binary_tree(root, mode).strip().split())
+
+def stringify_binary_tree(root: BinaryNode) -> str: 
+    r'''
+    Convert the given binary tree into the recursive form: Root(LeftSubTree)(RightSubTree)
+    '''
+    if root is None: 
+        return ''
+    return f'{root.value}({stringify_binary_tree(root.left)})({stringify_binary_tree(root.right)})'
+
+def generate_random_binary_tree(height: int) -> BinaryNode: 
+    raise NotImplementedError('TODO: generate_random_tree')
