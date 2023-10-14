35a2a40ba34fd6cfc5183009d27ca6915aeb3cc6
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-10-14 22:50:42 +0900

    automate Task2 test

--- python/general_tree.py
+++ python/general_tree.py
@@ -1,13 +1,16 @@
-from typing import TypeVar, Generic, Optional, List
+from typing import TypeVar, Generic, Optional, List, Tuple
 import argparse
 from random import randint
+import copy
 from binary_tree import *
+
+CASE_SEP = '**** ****'
 
 T = TypeVar('T')
 class Node(Generic[T]): 
-    def __init__(self, value: Optional[T]=None, left: Optional['Node[T]']=None, right: Optional['Node[T]']=None) -> None:
+    def __init__(self, value: Optional[T]=None, children: Optional[List['Node[T]']]=None) -> None:
         self.value: Optional[T] = value
-        self.children: List[Node[T]] = []
+        self.children: List[Node[T]] = [] if children is None else children.copy()
     
     def num_children(self) -> int:
         return len(self.children)
@@ -41,35 +44,43 @@ def stringify_tree(root: Node) -> str:
         return f'{root.value}({stringify_tree(root.children[0])})'
     return f'{root.value}({")(".join([stringify_tree(child) for child in root.children])})'
 
-def left_child_right_sibling(root: Node) -> BinaryNode: 
-    if not root: 
+def generate_random_tree(height: int, _range: Tuple[int, int]=(0, 9), _range_children: Tuple[int, int]=(0, 4)) -> Node: 
+    if height == 0: 
         return None
-    if not root.left: 
+    root = Node(randint(*_range))
+    n_children = randint(*_range_children)
+    if height == 1: 
         return root
-    left_child_right_sibling(root.left).right = root.right
-    root.right = None
-    return left_child_right_sibling(root.left)
+    for _ in range(n_children):
+        root.add_child(generate_random_tree(height - 1, _range))
+    return root
 
-def binary_tree_to_general_tree(root: BinaryNode) -> Node:
-    if not root: 
+def general_tree_to_lcrs(root: Optional[Node]) -> Optional[BinaryNode]: 
+    if root is None: 
         return None
-    node = Node(root.value)
-    if root.left: 
-        node.add_child(binary_tree_to_general_tree(root.left))
-    if root.right: 
-        node.add_child(binary_tree_to_general_tree(root.right))
-    return node
+    lcrs_children = [k for child in root.children if (k := general_tree_to_lcrs(child)) is not None]
+    if len(lcrs_children) == 0: 
+        return BinaryNode(root.value)
+    if len(lcrs_children) == 1:
+        return BinaryNode(root.value, left=lcrs_children[0])
+    for i in range(len(lcrs_children) - 1, 0, -1): 
+        lcrs_children[i - 1].right = lcrs_children[i]
+    return BinaryNode(root.value, left=lcrs_children[0])
 
 def main(args: argparse.Namespace) -> None: 
-    trees = []
+    cases = []
     for _ in range(args.N):
-        pass
-    print('\n'.join([f'{tup[0]}\n{tup[1]}' for tup in trees]))
+        height = randint(args.min_height, args.max_height)
+        general_root = generate_random_tree(height, _range=(0, 9), _range_children=(0, 5))
+        binary_root = general_tree_to_lcrs(general_root)
+        cases.append((stringify_binary_tree(binary_root), stringify_tree(general_root)))
+    print((f'\n{CASE_SEP}\n').join([f'{tup[0]}\n{tup[1]}' for tup in cases]))
+    print(CASE_SEP)
 
 if __name__ == '__main__': 
     parser = argparse.ArgumentParser(description='General Tree')
-    parser.add_argument('--N', type=int, default=10, help='Number of trees to generate')
-    parser.add_argument('--min-height', '-m', type=int, default=1, help='Minimum height of the tree')
-    parser.add_argument('--max-height', '-M', type=int, default=10, help='Maximum height of the tree')
+    parser.add_argument('-N', type=int, required=True, help='Number of trees to generate')
+    parser.add_argument('--min-height', '-m', type=int, required=True, help='Minimum height of the tree')
+    parser.add_argument('--max-height', '-M', type=int, required=True, help='Maximum height of the tree')
     args = parser.parse_args()
     main(args)
