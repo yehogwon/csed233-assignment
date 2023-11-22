27f09de1c157288d7b221e4f9138ad4562f666e4
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-11-22 12:21:36 +0900

    removed unnecessary code lines

--- python/open_hash.py
+++ python/open_hash.py
@@ -3,42 +3,6 @@ from random import randint, choices, random
 
 CASE_SEP = '**** ****'
 
-def merge(l1: list, l2: list) -> list: 
-    out = []
-    i = 0
-    j = 0
-    while i < len(l1) and j < len(l2):
-        if l1[i] <= l2[j]:
-            out.append(l1[i])
-            i += 1
-        else: 
-            out.append(l2[j])
-            j += 1
-    while i < len(l1):
-        out.append(l1[i])
-        i += 1
-    while j < len(l2):
-        out.append(l2[j])
-        j += 1
-    return out
-
-def concate_list(l: list[list]) -> str: 
-    return [str(elem) for elem in sum(l, [])]
-
-def merge_sort(l: list) -> list[str]: 
-    l = l.copy()
-    process: list[str] = []
-    process.append(' '.join([str(elem) for elem in l]))
-    
-    div_lists = [[elem] for elem in l]
-    while len(div_lists) > 1:
-        for i in range(0, len(div_lists), 2):
-            if i + 1 < len(div_lists):
-                div_lists[i] = merge(div_lists[i], div_lists[i+1])
-        div_lists = [div_lists[i] for i in range(0, len(div_lists), 2)]
-        process.append(' '.join(concate_list(div_lists)))
-    return process
-
 def main(args: argparse.Namespace) -> None:
     cases = []
     for _ in range(args.N):
